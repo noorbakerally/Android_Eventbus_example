ec93353b47ff507c87f2352e67ae65ef8a01ea65
Noorani Bakerally <noorani.bakerally@gmail.com>
2014-03-12 19:49:50 +0400

    List Working

--- src/com/example/eventbus_test/MainActivity.java
+++ src/com/example/eventbus_test/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.eventbus_test;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView listView1 = (ListView)findViewById(R.id.listView1);
+		listView1.setAdapter(new ArrayAdapter<String>(this, R.layout.row_item, Proxy.items));
 	}
 
 	@Override
---
+++ src/com/example/eventbus_test/Proxy.java
@@ -0,0 +1,8 @@
+package com.example.eventbus_test;
+
+public class Proxy {
+	public static String items [] = new String[] { "a","b"};
+	
+	
+
+}
